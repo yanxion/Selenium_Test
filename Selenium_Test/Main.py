09745472690e887534810cfa43dcf79b4b82797e
yanxion <yanxionmouse@gmail.com>
2017-07-05 15:59:51 +0800

    4

--- Selenium_Test/Main.py
+++ Selenium_Test/Main.py
@@ -7,19 +7,19 @@ import time
 
 chrome_path = "C:\Users\P17179\PycharmProjects\Selenium\chromedriver.exe"
 web = webdriver.Chrome(chrome_path)
+web.maximize_window()
+
 
 web.get('https://www.pixnet.net/blog/articles/category/19/hot/2')
 
-#print web.page_source
-#res = pq(web.page_source)
+
+res = pq(web.find_element_by_xpath("//*").get_attribute("outerHTML"))
+
+print res('.rank-11').find('h3').find('a').text()
 
 
-res = pq( 'https://www.pixnet.net/blog/articles/category/19/hot/2', encoding="utf-8")
-
-print res
-time.sleep(5)
-#print res('li.rank-11').find('h3').find('a').text()
 
 
-#web.find_element_by_link_text(u"下一頁").click()
-web.close()
+
+web.quit()
+#web.close()
